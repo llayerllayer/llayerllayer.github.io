05630e2cc073f431b1ba29f7feacca2fc64c5f03
arjan guerrero <53836612+arjanguerrero@users.noreply.github.com>
2025-07-26 17:37:20 +0100

    first draft of web design :D

---
+++ scripts.js
@@ -0,0 +1,5 @@
+﻿// Toggle contact details popup
+document.getElementById("contactBtn").addEventListener("click", () => {
+    const popup = document.getElementById("contactPopup");
+    popup.classList.toggle("hidden");
+});
